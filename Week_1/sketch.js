3f104413ce910907193fcd11d4f49ee7b37d0bd5
Brett Stiller <brettstiller@mac.com>
2015-09-18 10:11:49 -0400

    modified files

--- Week_1/sketch.js
+++ Week_1/sketch.js
@@ -6,17 +6,16 @@ var r = new Rune({
 });
 
 r.circle(400,300,100,100)
-.fill(0)
-.stroke(0)
+	.fill(0)
+	.stroke(0)
 
 r.rect(290,290,170,120)
-.fill(255)
-.stroke(false)
+	.fill(255)
+	.stroke(false)
 
 r.triangle(300, 300, 300, 600, 450, 340)
-  .fill(0)
-  .stroke(0)
-
+  	.fill(0)
+  	.stroke(0)
 
 
 r.draw();
