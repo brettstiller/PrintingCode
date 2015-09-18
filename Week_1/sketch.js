cb0decb067909a71b39611db148b7825f3e37147
Brett Stiller <brettstiller@mac.com>
2015-09-18 10:45:03 -0400

    Updated Code & Image

--- Week_1/sketch.js
+++ Week_1/sketch.js
@@ -9,13 +9,14 @@ r.circle(400,300,100,100)
 	.fill(0)
 	.stroke(0)
 
-r.rect(290,290,170,120)
+r.rect(290,340,170,80)
 	.fill(255)
 	.stroke(false)
 
-r.triangle(300, 300, 300, 600, 450, 340)
+r.triangle(300, 350, 300, 650, 450, 350)
   	.fill(0)
   	.stroke(0)
+
 
 
 r.draw();
